e9e1507fc7a9031877a2b3b867aa88b380485126
WoodyHenderson <woodyhenderson@hotmail.com>
2026-07-06 11:28:21 +0100

    Created the scaffolding for the express app and created a working /health page returning a basic JSON object.

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+
+const PORT = 3000;
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Hello, World!");
+});
+
+app.get("/health", (req, res) => {
+    res.json({ status: "UP", timestamp: new Date().toISOString() });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
