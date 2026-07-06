fe590dcb1e1fa377855d77ac0bc7b1dbfbb3407f
WoodyHenderson <woodyhenderson@hotmail.com>
2026-07-06 16:10:24 +0100

    Lint fixes.

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,10 @@ const app = express();
 app.use(express.json());
 
 app.get("/health", (_req, res) => {
-    const status = "UP";
-    res.json({ status, timestamp: new Date().toISOString() });
+	const status = "UP";
+	res.json({ status, timestamp: new Date().toISOString() });
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+	console.log(`Server is running on http://localhost:${PORT}`);
 });
