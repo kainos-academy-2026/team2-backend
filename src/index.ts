7f3e1f7dc37cbe258505214b30947902dd5fbdfe
HollyMckibbin <holly.mckibbin@kainos.com>
2026-07-06 16:22:02 +0100

    Backend test example cases

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,5 @@
-import express from "express";
-
 const PORT = 3000;
-const app = express();
-app.use(express.json());
-
-app.get("/health", (req, res) => {
-    res.json({ status: "UP", timestamp: new Date().toISOString() });
-});
+import { app } from "./app.js";
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
