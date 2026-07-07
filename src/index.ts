efc2904453cb933f8ca0e9af66d5e071c5c2425d
zoeoneillx <oneillzoe60@gmail.com>
2026-07-07 13:54:56 +0100

    Adding CI/CD Pipeline

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,6 @@
-import express from "express";
+import { app } from "./app.js";
 
 const PORT = 3000;
-const app = express();
-app.use(express.json());
-
-app.get("/health", (_req, res) => {
-	const status = "UP";
-	res.json({ status, timestamp: new Date().toISOString() });
-});
 
 app.listen(PORT, () => {
 	console.log(`Server is running on http://localhost:${PORT}`);
