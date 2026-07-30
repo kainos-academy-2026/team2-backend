46431093c19b099c1740ceda43557e023f7c50f8
DamianMcN <damianmcneice@hotmail.com>
2026-07-30 13:52:41 +0100

    Fixed terraform plan issue

--- my-infrastructure/keyvault.tf
+++ my-infrastructure/keyvault.tf
@@ -2,7 +2,7 @@ resource "azurerm_key_vault" "this" {
   name                       = "${local.name_prefix}-kv"
   location                   = module.resource_group.location
   resource_group_name        = module.resource_group.name
-  tenant_id                  = var.tenant_id
+  tenant_id                  = data.azurerm_client_config.current.tenant_id
   sku_name                   = "standard"
   soft_delete_retention_days = 7
   purge_protection_enabled   = false
--- my-infrastructure/variables.tf
+++ my-infrastructure/variables.tf
@@ -51,16 +51,6 @@ variable "tags" {
   }
 }
 
-variable "tenant_id" {
-  description = "Azure Active Directory tenant ID"
-  type        = string
-}
-
-variable "subscription_id" {
-  description = "Azure subscription ID"
-  type        = string
-}
-
 variable "acr_name" {
   description = "Name of the existing Azure Container Registry"
   type        = string
