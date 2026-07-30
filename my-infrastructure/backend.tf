a855855a67a41d866ce9a3cca3474c12beb62034
HollyMckibbin <hollymck15@outlook.com>
2026-07-30 09:54:51 +0100

    Task2 part b (#23)
    * first attempt
    
    * second attempt
    
    * third attempt
    
    * removing unecessary validation
    
    * only using env and adding terraform plan on main branch

--- my-infrastructure/backend.tf
+++ my-infrastructure/backend.tf
@@ -1,9 +1,4 @@
 # backend.tf
 terraform {
-  backend "azurerm" {
-    resource_group_name  = "rg-terraform-state"
-    storage_account_name = "tfstate1785248282" # Replace with your storage account name
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-  }
+  backend "azurerm" {}
 }
